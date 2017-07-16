99829d536ae8ac700259bc3b56f8f7087e8b5d00
ninolin <a7872278722@gmail.com>
2017-07-16 15:33:13 +0800

    first commit

---
+++ src/main/java/iii/myFirstServlet/App.java
@@ -0,0 +1,13 @@
+package iii.myFirstServlet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! aaa bbb" );
+    }
+}
